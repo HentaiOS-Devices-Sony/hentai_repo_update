9b809b12707b83616b872cfdf3cefaae055563b3
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-18 21:42:24 -0500

    Update hentai_repo_update.sh

--- hentai_repo_update.sh
+++ hentai_repo_update.sh
@@ -8,3 +8,4 @@ rm -rf external/exfat
 rm -rf vendor/codeaurora/telephony/internal
 rm -rf vendor/codeaurora/telephony/ims
 rm -rf hardware/qcom/sdm845/display/libdebug
+rm -rf hardware/qcom/sdm845/display/sdm/libs/utils
