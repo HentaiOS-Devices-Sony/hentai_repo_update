54e87e82d31489299f07e7d981c6e54c1f2219db
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-18 22:24:45 -0500

    Update hentai_repo_update.sh

--- hentai_repo_update.sh
+++ hentai_repo_update.sh
@@ -7,16 +7,5 @@ set -e
 rm -rf external/exfat
 rm -rf vendor/codeaurora/telephony/internal
 rm -rf vendor/codeaurora/telephony/ims
-rm -rf hardware/qcom/sdm845/display/libdebug
-rm -rf hardware/qcom/sdm845/display/sdm/libs/utils
-rm -rf hardware/qcom/sdm845/display/sdm/libs/core
-rm -rf hardware/qcom/sdm845/display/liblight
-rm -rf hardware/qcom/sdm845/display/libmemtrack
-rm -rf hardware/qcom/sdm845/display/hdmi_cec
-rm -rf hardware/qcom/sdm845/display/sdm/libs/hwc2
-rm -rf hardware/qcom/sdm845/display/gpu_tonemapper
-rm -rf hardware/qcom/sdm845/display/libdrmutils
-rm -rf hardware/qcom/sdm845/display/libdisplayconfig
-rm -rf hardware/qcom/sdm845/display/gralloc
 
 echo "all hentai patches applied successfully!"
