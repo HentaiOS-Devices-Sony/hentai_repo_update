b66d8e531b93e66567aa8487cc1bbbab8820dad5
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-02-29 18:22:02 -0500

    Update hentai_repo_update.sh

--- hentai_repo_update.sh
+++ hentai_repo_update.sh
@@ -7,5 +7,6 @@ rm -rf external/json-c
 rm -rf external/exfat
 rm -rf vendor/codeaurora/telephony/internal
 rm -rf vendor/codeaurora/telephony/ims
+rm -rf hardware/qcom-caf/common/fwk-detect
 
 echo "all hentai patches applied successfully!"
