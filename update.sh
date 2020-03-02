7cca1676e3e7779a5c2f12f6d179cb543346aead
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-03-02 15:05:51 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 repo sync -f --force-sync --no-tags --no-clone-bundle -j$(nproc --all)
 
-. repo_update.sh
+. vendor/oss/repo_update/repo_update.sh
 
 . vendor/oss/hentai_repo_update/hentai_repo_update.sh
