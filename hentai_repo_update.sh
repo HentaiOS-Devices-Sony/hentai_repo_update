de8ffdd7c5458880ac4c002951b2f103f181d7cd
乐正白芷 <42693943+yuezhengbaizhi@users.noreply.github.com>
2020-03-02 12:47:49 -0500

    Update hentai_repo_update.sh

--- hentai_repo_update.sh
+++ hentai_repo_update.sh
@@ -3,6 +3,8 @@
 # exit script immediately if a command fails
 set -e
 
+rm -rf device/ti/beagle_x15
+rm -rf device/ti/beagle_x15-kernel
 rm -rf external/json-c
 rm -rf external/exfat
 rm -rf vendor/codeaurora/telephony/internal
